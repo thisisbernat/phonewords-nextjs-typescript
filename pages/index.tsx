c35fe43ade520df74eb84c7b800af48bd3afc4dd
Bernat Escarrà <bernat.escarra@gmail.com>
2022-09-16 12:45:50 +0200

    Change text on buttons

--- pages/index.tsx
+++ pages/index.tsx
@@ -63,15 +63,15 @@ const Home: NextPage = () => {
         columnGap="1rem"
         columns="repeat(3, 1fr)"
       >
-        <Button width={"4.5rem"} disabled={true}>1<br /><Text as="span" type="white" italic={true} size="small">_</Text></Button>
-        <Button width={"4.5rem"} onClick={() => writeToDisplay('2')}>2<br /><Text as="span" type="white" italic={true} size="small">ABC</Text></Button>
-        <Button width={"4.5rem"} onClick={() => writeToDisplay('3')}>3<br /><Text as="span" type="white" italic={true} size="small">DEF</Text></Button>
-        <Button width={"4.5rem"} onClick={() => writeToDisplay('4')}>4<br /><Text as="span" type="white" italic={true} size="small">GHI</Text></Button>
-        <Button width={"4.5rem"} onClick={() => writeToDisplay('5')}>5<br /><Text as="span" type="white" italic={true} size="small">JKL</Text></Button>
-        <Button width={"4.5rem"} onClick={() => writeToDisplay('6')}>6<br /><Text as="span" type="white" italic={true} size="small">MNO</Text></Button>
-        <Button width={"4.5rem"} onClick={() => writeToDisplay('7')}>7<br /><Text as="span" type="white" italic={true} size="small">PQRS</Text></Button>
-        <Button width={"4.5rem"} onClick={() => writeToDisplay('8')}>8<br /><Text as="span" type="white" italic={true} size="small">TUV</Text></Button>
-        <Button width={"4.5rem"} onClick={() => writeToDisplay('9')}>9<br /><Text as="span" type="white" italic={true} size="small">WXYZ</Text></Button>
+        <Button width={"4.5rem"} disabled={true}>1<br /><Text as="span" type="white" italic={true}>_</Text></Button>
+        <Button width={"4.5rem"} onClick={() => writeToDisplay('2')}>2<br /><Text as="span" type="white" italic={true}>abc</Text></Button>
+        <Button width={"4.5rem"} onClick={() => writeToDisplay('3')}>3<br /><Text as="span" type="white" italic={true}>def</Text></Button>
+        <Button width={"4.5rem"} onClick={() => writeToDisplay('4')}>4<br /><Text as="span" type="white" italic={true}>ghi</Text></Button>
+        <Button width={"4.5rem"} onClick={() => writeToDisplay('5')}>5<br /><Text as="span" type="white" italic={true}>jkl</Text></Button>
+        <Button width={"4.5rem"} onClick={() => writeToDisplay('6')}>6<br /><Text as="span" type="white" italic={true}>mno</Text></Button>
+        <Button width={"4.5rem"} onClick={() => writeToDisplay('7')}>7<br /><Text as="span" type="white" italic={true}>pqrs</Text></Button>
+        <Button width={"4.5rem"} onClick={() => writeToDisplay('8')}>8<br /><Text as="span" type="white" italic={true}>tuv</Text></Button>
+        <Button width={"4.5rem"} onClick={() => writeToDisplay('9')}>9<br /><Text as="span" type="white" italic={true}>wxyz</Text></Button>
         <Button width={"4.5rem"} onClick={clearDisplay}>Clear</Button>
         <Button width={"4.5rem"} disabled={true}>0</Button>
         <Button width={"4.5rem"} onClick={backspace}><ChevronLeft /></Button>
