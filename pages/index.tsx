02f08f4102a5e937e058f84e7064f3adf07da272
Bernat Escarrà <bernat.escarra@gmail.com>
2022-09-16 17:03:41 +0200

    Update Loading component

--- pages/index.tsx
+++ pages/index.tsx
@@ -28,11 +28,11 @@ const Home: NextPage = () => {
         <title>T9 Emulator 🥝</title>
         <meta name="description" content="Phonewords" />
         <link rel="icon" href="/favicon.ico" />
-      </Head>
+      </Head>    
       <div className={styles.screen}>
         {message ? message : <div> </div>}
         <span className={styles.predictedDisplay}>
-          {isLoading ? <Loading customSize={20} /> : words.map((word, i) => word && <div key={`pred-${i}`} onClick={() => updateScreen(word)} style={{ cursor: "pointer" }}><BadgePrimitive background="#007F6D" foregroundColor="#fff">{word}</BadgePrimitive></div>)}
+          {isLoading ? <BadgePrimitive background="#007F6D" foregroundColor="#fff"><Loading type="inlineLoader"/></BadgePrimitive> : words.map((word, i) => word && <div key={`pred-${i}`} onClick={() => updateScreen(word)} style={{ cursor: "pointer" }}><BadgePrimitive background="#007F6D" foregroundColor="#fff">{word}</BadgePrimitive></div>)}
         </span>
       </div>
       <div className={styles.digitsDisplay}>{digits}<span className={styles.blink}>_</span></div>
