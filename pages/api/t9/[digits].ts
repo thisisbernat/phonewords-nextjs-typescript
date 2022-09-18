51fd1ae06ba4b1c2cbd0e861ca3cba52cd350712
Bernat Escarrà <bernat.escarra@gmail.com>
2022-09-18 21:09:51 +0200

    Add request validation

--- pages/api/t9/[digits].ts
+++ pages/api/t9/[digits].ts
@@ -2,6 +2,16 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import { t9 } from '../../../services/t9.services'
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method !== 'GET') {
+    res.status(405).json({ err: 'Method not allowed' })
+    return
+  }
+
+  if (typeof req.query.digits !== 'string') {
+    res.status(400).json({ err: 'Invalid digits parameter' })
+    return
+  }
+
   const { digits } = req.query
   const wordsArray = t9(String(digits))
   res.status(200).json(wordsArray)
