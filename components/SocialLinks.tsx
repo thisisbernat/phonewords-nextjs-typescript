319889c2b7eb962c629d2b7b0011bddaa1a95973
Bernat Escarrà <bernat.escarra@gmail.com>
2022-09-19 16:09:14 +0200

    Update SocialLinks styles

--- components/SocialLinks.tsx
+++ components/SocialLinks.tsx
@@ -1,12 +1,13 @@
-import Button from "@kiwicom/orbit-components/lib/Button"
+import styles from '../styles/SocialLinks.module.css'
 import Github from "@kiwicom/orbit-components/lib/icons/Github"
 import Linkedin from "@kiwicom/orbit-components/lib/icons/Linkedin"
+import TextLink from "@kiwicom/orbit-components/lib/TextLink"
 
 const SocialLinks = () => {
   return (
-    <div>
-      <Button type="white" iconLeft={<Github />} href="https://github.com/thisisbernat/phonewords-nextjs-typescript" external={true}>GitHub</Button>
-      <Button type="white" iconLeft={<Linkedin />} href="https://www.linkedin.com/in/bernatescarra/" external={true}>LinkedIn</Button>
+    <div className={styles.links}>
+      <TextLink href="https://github.com/thisisbernat/phonewords-nextjs-typescript" external={true}><Github /></TextLink>
+      <TextLink href="https://www.linkedin.com/in/bernatescarra/" external={true}><Linkedin /></TextLink>
     </div>
   )
 }
